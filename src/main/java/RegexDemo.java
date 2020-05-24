b289c7a0543c021e24c4256f78079f093367680b
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-05-24 22:08:38 +0200

    First "working" test

---
+++ src/main/java/RegexDemo.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Copyright 2020 by Bajupa.com
+ * Created by peter on 24.05.20.
+ */
+public class RegexDemo {
+    public static Matcher getCommentMatcherFor(String givenString) {
+        Pattern pattern = Pattern.compile("");
+        return pattern.matcher(givenString);
+    }
+}
---
+++ src/test/java/RegexDemoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Copyright 2020 by Bajupa.com
+ * Created by peter on 24.05.20.
+ */
+class RegexDemoTest {
+    @Test
+    void itShouldMatchAnEmptyComment() {
+        // arrange
+        Matcher matcher = RegexDemo.getCommentMatcherFor("/**/");
+
+        // act
+        boolean groupFound = matcher.find();
+        assertTrue(groupFound);
+    }
+}
